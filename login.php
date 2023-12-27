e949be986dafa632fba0fabfe703bd15c9665dbb
MARK SAFWAT TANIOUS FILIBS <mark.filibs@student.aast.edu>
2023-12-27 18:26:43 +0200

    fixed some bugs

--- login.php
+++ login.php
@@ -4,16 +4,33 @@ include 'db.php';
 
 // Handle user login
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $username = mysqli_real_escape_string($con, $_POST['username']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
 
     $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $result = mysqli_query($con, $query);
 
     if (mysqli_num_rows($result) == 1) {
-        echo "<script type='text/javascript'> alert('Login successful')</script>";
+        // Redirect to index.html on successful login
+        header("Location: index.html");
+        exit();
     } else {
-        echo "<script type='text/javascript'> alert('Invalid username or password')</script>";
+        // Redirect to login.html if login is not successful
+        header("Location: login.html");
+        exit();
     }
 }
 ?>
+<?php
+    // Start the session to access session variables
+    session_start();
+
+    // Check if the user is logged in
+    if (isset($_SESSION['username'])) {
+        // User is logged in, display a welcome message or other content
+        echo '<p>Welcome, ' . $_SESSION['username'] . '!</p>';
+    } else {
+        // User is not logged in, display signup button
+        echo '<button id="signupButton">Sign Up</button>';
+    }
+    ?>
