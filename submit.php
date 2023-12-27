53d4a862cb9b5734acf7246bbf83372b51c8a244
MARK SAFWAT TANIOUS FILIBS <mark.filibs@student.aast.edu>
2023-12-27 15:18:18 +0200

    secured the php file from xss attack and html tags

--- submit.php
+++ submit.php
@@ -8,12 +8,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $phoneNumber = $_POST['phoneNumber'];
 
         // Perform validation (you can add more validation as needed)
-        if (empty($name) || empty($phoneNumber) || strlen($phoneNumber) !== 11 || !is_numeric($phoneNumber)) {
-            throw new Exception('Invalid form data.');
+        if (empty($name) && empty($phoneNumber)) {
+            throw new Exception('Please provide a name and a 11-digit numeric phone number.');
         }
+
+        if (empty($name)) {
+            throw new Exception('Please provide a valid name.');
+        }
+
+        // Check for HTML tags in the name
+        if (strip_tags($name) !== $name) {
+            throw new Exception('HTML tags are not allowed in the name.');
+        }
+
+        if (empty($phoneNumber) || strlen($phoneNumber) !== 11 || !is_numeric($phoneNumber)) {
+            throw new Exception('Please provide a 11-digit numeric phone number.');
+        }
+
+        // Escape user input before using it in HTML
+        $escapedName = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+        $escapedPhoneNumber = htmlspecialchars($phoneNumber, ENT_QUOTES, 'UTF-8');
 
         // Process the form data (replace this with your specific logic)
         // For demonstration purposes, just echoing the data
+        
         echo json_encode(array('status' => 'success', 'message' => 'Form submitted successfully.'));
         exit();
     } catch (Exception $e) {
